df9d564fa1a570bf4c656e4fbe0c912b17def23d
Fabrice Bouyé <fabrice.bouye@gmail.com>
2015-06-01 22:14:27 +1100

    Added missing javadoc.

--- src/test/query/ColorsQuery.java
+++ src/test/query/ColorsQuery.java
@@ -21,6 +21,11 @@ public enum ColorsQuery {
 
     private static final String BASECODE = "https://api.guildwars2.com/v2/colors"; // NOI18N.
 
+    /**
+     * Récupère la liste des indenfiants des teintures.
+     * @return Une {@code List<Integer>}, jamais {@code null}.
+     * @throws IOException En cas d'erreur.
+     */
     public static List<Integer> list() throws IOException {
         final JsonArray array = queryArray(BASECODE);
         final List<Integer> result = array.getValuesAs(JsonNumber.class)
@@ -30,12 +35,26 @@ public enum ColorsQuery {
         return Collections.unmodifiableList(result);
     }
 
+    /**
+     * Récupère l'objet teinture.
+     * @param languageCode Le code du language.
+     * @param id L'identifiant de la teinture.
+     * @return Une instance de {@code Dye}, jamais {@code null}.
+     * @throws IOException En cas d'erreur.
+     */
     public static Dye dyeInfo(final String languageCode, final int id) throws IOException {
         final JsonObject value = QueryUtils.queryObject(String.format("%s/%d?lang=%s", BASECODE, id, languageCode)); // NOI18N.
         final Dye result = DyeFactory.createDyeFromJSON(value);
         return result;
     }
 
+    /**
+     * Récupère plusieurs objets teintures.
+     * @param languageCode Le code du language.
+     * @param ids Les identifiants des teintures. Si aucun indentfiant est fourni, toutes les teintures connues seront retournées.
+     * @return Une {@code List<Dye>}, jamais {@code null}.
+     * @throws IOException En cas d'erreur.
+     */
     public static List<Dye> dyeInfos(final String languageCode, final int... ids) throws IOException {
         final String idsCode = idsToString(ids);
         final JsonArray array = queryArray(String.format("%s?ids=%s&lang=%s", BASECODE, idsCode, languageCode)); // NOI18N.
